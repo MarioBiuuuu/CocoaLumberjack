88cd7fa552f9aa87b2bc8fb62dba5c32927eb35f
Bogdan Poplauschi <bpoplauschi@gmail.com>
2018-10-22 16:04:05 +0300

    Cleanup phase 2: - working macOSSwift integration - cleanup settings from Integration

---
+++ Integration/macOSSwiftIntegration/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  macOSSwiftIntegration
+//
+//  Created by Bogdan Poplauschi on 18/10/2018.
+//  Copyright © 2018 CocoaLumberjack. All rights reserved.
+//
+
+import Foundation
+import CocoaLumberjackSwift
+
+DDLog.add(DDOSLogger.sharedInstance)
+DDLogVerbose("Verbose")
+DDLogInfo("Info")
+DDLogWarn("Warn")
+DDLogError("Error")
